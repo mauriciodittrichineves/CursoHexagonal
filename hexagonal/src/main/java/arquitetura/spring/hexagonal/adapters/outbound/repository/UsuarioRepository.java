37df3b7cc7ec09632b8e087cf2143c6e16c3e22c
mauriciodittrichineves <34429909+mauriciodittrichineves@users.noreply.github.com>
2022-08-01 17:33:42 -0300

    Add files via upload

---
+++ hexagonal/src/main/java/arquitetura/spring/hexagonal/adapters/outbound/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package arquitetura.spring.hexagonal.adapters.outbound.repository;
+
+import arquitetura.spring.hexagonal.adapters.inbound.entity.UsuarioEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
+
+}
+
